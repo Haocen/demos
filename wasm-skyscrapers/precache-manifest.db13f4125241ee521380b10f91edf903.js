7fc7f78ec072a308bd9e4ad6ed55932976b37f48
Haocen Xu <admin@haocen.me>
2018-11-07 15:35:45 -0500

    Add demo for wasm-skyscrapers.

---
+++ wasm-skyscrapers/precache-manifest.db13f4125241ee521380b10f91edf903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b0c9554fb5b89155834",
+    "url": "/demos/wasm-skyscrapers/static/js/runtime~main.4b0c9554.js"
+  },
+  {
+    "revision": "681c4ad3d6c892b47ea6",
+    "url": "/demos/wasm-skyscrapers/static/js/main.681c4ad3.chunk.js"
+  },
+  {
+    "revision": "2988b7ef76b670386397",
+    "url": "/demos/wasm-skyscrapers/static/js/1.2988b7ef.chunk.js"
+  },
+  {
+    "revision": "681c4ad3d6c892b47ea6",
+    "url": "/demos/wasm-skyscrapers/static/css/main.c469c314.chunk.css"
+  },
+  {
+    "revision": "2988b7ef76b670386397",
+    "url": "/demos/wasm-skyscrapers/static/css/1.008d2407.chunk.css"
+  },
+  {
+    "revision": "6a0aa1ba149af27c2d90f16d968ef4d8",
+    "url": "/demos/wasm-skyscrapers/index.html"
+  }
+];
---
+++ wasm-skyscrapers/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/demos/wasm-skyscrapers/precache-manifest.db13f4125241ee521380b10f91edf903.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/demos/wasm-skyscrapers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
